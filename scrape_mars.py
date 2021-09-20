74acab676016bde78fd801c46fd9b06ff95c8070
abednarz210 <86257908+abednarz210@users.noreply.github.com>
2021-09-20 17:07:51 -0500

    scrape

--- scrape_mars.py
+++ scrape_mars.py
@@ -1,51 +1,71 @@
+import requests
+import pymongo
+import pandas as pd
+
+   
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import time
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
 
 
-def scrape_info():
+def scrape():
     # Set up Splinter
-    executable_path = {'executable_path': ChromeDriverManager().install()}
+    executable_path = {'executable_path': '/Users/amybednarz/Downloads/chromedriver'}
     browser = Browser('chrome', **executable_path, headless=False)
 
-    #featured image 
-    # Example:
-    featured_image_url = 'https://spaceimages-mars.com/image/featured/mars2.jpg'
+#NASA Mars News 
+
+    mars_url = "https://mars.nasa.gov/news/"
+    browser.visit(mars_url)
+
+    html = browser.html
+    mars_news_soup= bs(html,'html.parser')
+
+#find title and paragraph
+
+    mars_title = mars_news_soup.body.find("div", class_= "content_title")
+    mars_paragraph = mars_news_soup.body.find("div", class_= "article_teaser_body")
+    print(mars_title)
+    print(mars_paragraph)
 
 
-    # Visit visitcostarica.herokuapp.com
-    url = "https://visitcostarica.herokuapp.com/"
-    browser.visit(url)
+    #JPL Mars Space Images - Featured Image
+    mars_image_url = "https://spaceimages-mars.com/"
+    browser.visit(mars_image_url)
 
-    time.sleep(1)
+
+    # Mars Facts
+    mars_facts_url = "https://galaxyfacts-mars.com/"
+    mars_table =  pd.read_html(mars_facts_url)
+    mars_table
+    #time.sleep(5)
+
+    mars_table_df = mars_table[1]
+
+    mars_table_df.columns = ['Describe', 'Values']
+    mars_table_df
+
+    mars_table_html = mars_table_df.to_html()
+    print(mars_table_html)
 
     # Scrape page into Soup
     html = browser.html
     soup = bs(html, "html.parser")
 
-    # Get the average temps
-    avg_temps = soup.find('div', id='weather')
+    #Mars Hemispheres
+    mars_hemisphere_url = "https://marshemispheres.com/"
+    browser.visit(mars_hemisphere_url)
 
-    # Get the min avg temp
-    min_temp = avg_temps.find_all('strong')[0].text
+    # Mars Dictionary 
+    mars_hemisphere_url_dict = [
+        {"title": "Valles Marineris Hemisphere", "img_url_vm": "https://marshemispheres.com/images/valles_marineris_enhanced-full.jpg"},
+         {"title": "Cerberus Hemisphere", "img_url_ch": "https://marshemispheres.com/images/full.jpg"},
+        {"title": "Schiaparelli Hemisphere", "img_url_sh": "https://marshemispheres.com/images/schiaparelli_enhanced-full.jpg"},
+        {"title": "Syrtis Major Hemisphere", "img_url-smh": "https://marshemispheres.com/images/syrtis_major_enhanced-full.jpg"},
+    ]
 
-    # Get the max avg temp
-    max_temp = avg_temps.find_all('strong')[1].text
-
-    # BONUS: Find the src for the sloth image
-    relative_image_path = soup.find_all('img')[2]["src"]
-    sloth_img = url + relative_image_path
-
-    # Store data in a dictionary
-    costa_data = {
-        "sloth_img": sloth_img,
-        "min_temp": min_temp,
-        "max_temp": max_temp
-    }
-
-    # Close the browser after scraping
+    #quit browser
     browser.quit()
-
-    # Return results
-    return costa_data
+    return mars_hemisphere_url_dict 
+    
